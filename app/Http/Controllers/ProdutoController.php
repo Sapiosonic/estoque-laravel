bbe8065b0820914c1121845e42f1fe77f2d8f9f5
Radames <radames.rodrigues@exonsistemas.com.br>
2024-08-20 14:41:53 -0300

    feat (Produtos): Cria uma rota e um Controller para produtos
    Aqui será posteriormente a página inicial de produtos

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,14 @@
+<?php
+
+    namespace app\Http\Controllers;
+    use App\Http\Controllers\Controller;
+
+
+    class ProdutoController extends Controller
+    {
+        public function lista()
+        {
+            // nosso código vai aqui!
+            return  "<h1>Listagem de produtos com Laravel</h1>";
+        }
+    }
